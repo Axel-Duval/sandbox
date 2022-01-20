46bc24737fb770f88a19268b85e264a308e34fd1
Axel-Duval <axel.duval74@gmail.com>
2022-01-20 18:57:41 +0100

    📦 Provide starter sandbox

---
+++ starter/src/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello world</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        Unde laudantium, error soluta molestias neque at voluptatibus
+        inventore ad numquam, in maiores explicabo voluptate labore
+        quasi similique esse facere cum repellendus.
+      </p>
+    </>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
